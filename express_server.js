d857dc0aaa12b8bf5daa455c926441b8904fb320
Yufei Lan <lanyufei518@gmail.com>
2019-04-09 18:35:44 +0000

    url connected

--- express_server.js
+++ express_server.js
@@ -2,6 +2,15 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
