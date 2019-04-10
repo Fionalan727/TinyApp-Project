f15847c243142162009dc3e3a27144fb19359277
Yufei Lan <lanyufei518@gmail.com>
2019-04-10 22:58:15 +0000

    login option added

--- express_server.js
+++ express_server.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var app = express();
+
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
+
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
@@ -18,19 +23,27 @@ app.post("/urls", (req, res) => {
 
 //http://localhost:8080/urls
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 //http://localhost:8080/urls/new
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { username: req.cookies["username"],shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect("/urls")
+})
 
 //delete the urls 
 //redirect /urls
@@ -39,6 +52,7 @@ app.post("/urls/:shortURL/delete",(req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 })
+
 
 //edit the url to add new long URL
 //update the longurl to database and match with the original url
@@ -55,6 +69,8 @@ app.get("/u/:shortURL", (req, res) => {
   ? res.redirect(longURL) 
   : res.send(`${req.params.shortURL} is not a valid short URL`);
 });
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
