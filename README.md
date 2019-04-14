11e831cb163fb4b024aabc80e4652b7703bd54fd
Yufei Lan <lanyufei518@gmail.com>
2019-04-14 19:50:13 +0000

    README.md added, clean the code, comment added

---
+++ README.md
@@ -0,0 +1,23 @@
+# TinyApp Project
+
+TInyApp is a full web application built with Node and Express that allows usersto shorten long URLs.
+
+## Final Product
+
+
+!["screenshot description"](#)
+!["screenshot description"](#)
+
+
+## Dependencies 
+- Node.js
+-Express
+-EJS
+-bcrypt
+-body-parser
+-cookie-session
+
+## Getting Started
+
+-Install all dependencies (using the  `npm install ` command).
+-Run the developement web server using the node `node express_server.js` command.
--- express_server.js
+++ express_server.js
@@ -1,20 +1,27 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const cookieSession = require('cookie-session')
+const app = express();
 const bcrypt = require('bcrypt');
 
-var cookieParser = require('cookie-parser')
+app.use(cookieSession({
+  name: 'session',
+  keys: ["kneel!!!!!!!!!!"],
 
-app.use(cookieParser())
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
-var PORT = 8080; // default port 8080
+
+
+const PORT = 8080; 
 app.set("view engine", "ejs");
 
 //must before all the routes
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-//store and access the users in the app
 
+//store and access the users in the app
 const users = {
   "userRandomID": {
     id: "userRandomID", 
@@ -29,27 +36,39 @@ const users = {
 }
 
 
-//object used to store unique short url and long url
+
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
-//create a random string as a special key to store the url from input 
-//store into the urlDatabase
-//to urls.show and show website information
-app.post("/urls", (req, res) => {
-  let newKey = generateRandomString()
-  urlDatabase[newKey] = {};
-  urlDatabase[newKey]['longURL'] = req.body.longURL;
-  urlDatabase[newKey]['userID'] = req.cookies["user_id"]
-  
-  res.redirect(`/urls`);      
+
+app.listen(PORT, () => {
+});
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+
+app.get("/", (req, res) => {
+  let user_id = req.session.user_id;
+  if (!user_id) {
+    res.redirect("/login")
+  } else {
+    let templateVars = {
+      'user_id': user_id,
+      "urls": urlsForUser(user_id),
+      "email": (users[user_id] ? users[user_id].email : users[user_id])
+    };
+    res.render("urls_index", templateVars);
+  }
 });
 
-
-//http://localhost:8080/urls
+//check if user is logged in , if the user is logged in they can change their code only
 app.get("/urls", (req, res) => {
-  let user_id = req.cookies.user_id;
+  let user_id = req.session.user_id;
   if (!user_id) {
     res.redirect("/login")
   } else {
@@ -63,16 +82,33 @@ app.get("/urls", (req, res) => {
 
 });
 
+//endpoint of login page
+app.get("/login", (req,res) =>{
+  let templateVars = { 
+    user_id: req.session.user_id,
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
+  };
+  res.render("url_login",templateVars);
+})
 
+//endpoint for register page
+app.get("/register",(req, res) =>{
+  let templateVars = {
+    user_id: req.session.user_id,
+    email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
+  }
+  res.render("registration_page",templateVars);
+});
 
-
-//http://localhost:8080/urls/new
+//only logged in user can create new urls
 app.get("/urls/new", (req, res) => {
  
-  if(req.cookies["user_id"]){
+  if(req.session.user_id){
     let templateVars = {
-      user_id: req.cookies["user_id"],
-      email: (users[req.cookies["user_id"]] ? users[req.cookies["user_id"]].email : users[req.cookies["user_id"]])
+      user_id: req.session.user_id,
+      email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
 
     };
     res.render("urls_new",templateVars);
@@ -81,47 +117,51 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+//check user logged in or not, if yes they can update their urls
 app.get("/urls/:shortURL", (req, res) => {
-  if(req.cookies["user_id"]){
-    let templateVars = { user_id: req.cookies["user_id"],
+  if(req.session.user_id){
+    let templateVars = { 
+    user_id: req.session.user_id,
     shortURL: req.params.shortURL, 
     longURL: urlDatabase[req.params.shortURL],
-    email: (users[req.cookies["user_id"]] ? users[req.cookies["user_id"]].email : users[req.cookies["user_id"]])
+    email: (users[req.session.user_id] ? users[req.session.user_id].email : users[req.session.user_id])
     };
     res.render("urls_show", templateVars);
   }else{
     res.redirect("/login")
   }
-
-
-  
 });
 
-app.get("/login", (req,res) =>{
-  let templateVars = { 
-    user_id: req.cookies["user_id"],
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL],
-    email: (users[req.cookies["user_id"]] ? users[req.cookies["user_id"]].email : users[req.cookies["user_id"]])
+//redirect to the actual page
+//"/u/" is just a made up path so it doesn't conflict to the url
+app.get("/u/:shortURL", (req, res) => {
+  const url = urlDatabase[req.params.shortURL];
+  let longURL = url && url.longURL;
+  
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.send(`${req.params.shortURL} is not a valid short URL`);
   };
-  res.render("url_login",templateVars)
-})
-
-app.get("/register",(req, res) =>{
-  let templateVars = {
-    user_id: req.cookies["user_id"],
-    email: (users[req.cookies["user_id"]] ? users[req.cookies["user_id"]].email : users[req.cookies["user_id"]])
-  }
-  res.render("registration_page",templateVars)
-})
+});
 
 
+//created random id and use it as a key to store user input information to url database
+app.post("/urls", (req, res) => {
+  let newKey = generateRandomString()
+  urlDatabase[newKey] = {};
+  urlDatabase[newKey]['longURL'] = req.body.longURL;
+  urlDatabase[newKey]['userID'] = req.session.user_id;
+  
+  res.redirect(`/urls`);      
+});
 
+//make sure user fullfill register requirement 
 app.post("/register",(req, res) => {
-  const emailInput = req.body.email
-  const passwordInput = req.body.password
-  const newUserID = generateRandomString()
-  users[newUserID] = {}
+  const emailInput = req.body.email;
+  const passwordInput = req.body.password;
+  const newUserID = generateRandomString();
+  users[newUserID] = {};
 
 
   if(!emailInput || !passwordInput){
@@ -134,39 +174,35 @@ app.post("/register",(req, res) => {
     users[newUserID]["id"] = newUserID
     users[newUserID]["email"] = emailInput
     users[newUserID]["password"] = hasher(passwordInput); 
-    console.log("users", users);
-    console.log("register a user", users);
-    res.cookie("user_id", newUserID);
+    req.session.user_id = newUserID;
     res.redirect("/urls");
   }
 })
 
-
+//check login requirement
 app.post("/login", (req, res) => {
   const user = emailChecker(req.body.email)
-  console.log("user is", user);
   if(!user) {
     res.status(403)
     res.send("email does not exists! DNE!")
   }else if(!bcrypt.compareSync(req.body.password, user.password)) {
     res.status(403)
     res.send("password does not match,try again")
-  }else{
-     res.cookie("user_id", user["id"])
+  }else{f
+    req.session.user_id = user["id"];
     res.redirect('/urls');
 
   }
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id")
+  req.session = null;
   res.redirect("/urls")
 })
 
-//delete the urls 
-//redirect /urls
+//check login or not , if yes,user able to delete their own urls
 app.post("/urls/:shortURL/delete",(req, res) => {
-  if(req.cookies["user_id"]){
+  if(req.session.user_id){
     delete urlDatabase[req.params.shortURL]
     res.redirect("/urls")
   }else{
@@ -180,40 +216,15 @@ app.post("/urls/:shortURL/delete",(req, res) => {
 //update the longurl to database and match with the original url
 app.post("/urls/:shortURL/edit", (req,res) => {
   urlDatabase[req.params.shortURL]['longURL'] = req.body.longURL;
-  const cookieUserId = req.cookies['user_id'];
+  const cookieUserId = req.session.user_id;
   if (!cookieUserId) {
     res.send("Please login first")
   }
   res.redirect("/urls")
 })
 
-//redirect to the actual page
-//"/u/" is just a made up path so it doesn't conflict to the url
-app.get("/u/:shortURL", (req, res) => {
-  const url = urlDatabase[req.params.shortURL];
-  let longURL = url && url.longURL;
-  
-  if (longURL) {
-    res.redirect(longURL);
-  } else {
-    res.send(`${req.params.shortURL} is not a valid short URL`)
-  }
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-});
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
-
+//check if user's input email is match with the emails saved in database
 function emailChecker (email){
   for (id in users) {
     if (email === users[id].email){
@@ -232,6 +243,8 @@ function generateRandomString() {
   }
   return text;
 }
+
+//find the urls made by  current user only
 function urlsForUser(id){
   let result = {};
   for(let shortURL in urlDatabase){
@@ -242,6 +255,7 @@ function urlsForUser(id){
   }
   return result;
 }
+
 function hasher(password) {
   const hashedPassword = bcrypt.hashSync(password, 10);
   return hashedPassword;
